63439f247bc1e06d636bef0f648429f9033561ea
Hemavathi2102 <hemavathi2102@gmail.com>
2026-05-10 23:00:47 +0530

    updated

--- server.js
+++ server.js
@@ -94,7 +94,9 @@ const Vote = mongoose.model("Vote", voteSchema);
 
 // ✅ EMAIL SETUP
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
@@ -160,12 +162,17 @@ app.post("/register", async (req, res) => {
 
 // send email AFTER response
     transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to: email,
-    subject: "OTP",
-    text: `OTP: ${otp}`
-    }).catch(err => console.log("Mail failed:", err.message));
-
+  from: process.env.EMAIL_USER,
+  to: email,
+  subject: "OTP",
+  text: `OTP: ${otp}`
+})
+.then(info => {
+  console.log("📧 Email sent:", info.response);
+})
+.catch(err => {
+  console.log("❌ Email error:", err);
+});
   } catch (err) {
     console.error("REGISTER ERROR:", err);
 
