13e8453475e221285298067ed3996b66f3faa72c
Hemavathi2102 <hemavathi2102@gmail.com>
2026-05-12 08:26:30 +0530

    Fixed Gmail SMTP timeout

--- server.js
+++ server.js
@@ -94,13 +94,12 @@ const Vote = mongoose.model("Vote", voteSchema);
 
 // ✅ EMAIL SETUP
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  }
+  },
+  connectionTimeout: 10000,
 });
 
 let electionEnded = false; 
